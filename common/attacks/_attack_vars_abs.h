5fc8e96fca8971b2661799e1d8307132168264d1
onehouwong <onehouwong@gmail.com>
2024-06-22 23:50:28 -0400

    minor

--- common/attacks/_attack_vars_abs.h
+++ common/attacks/_attack_vars_abs.h
@@ -4,7 +4,7 @@
 _ATTACK_VDEF(int, bts_attack, 0)          // <level information; openair2/RRC/LTE/{rrc_UE.c,L2_interface_ue.c}; openair3/NAS/UE/EMM/SAP/{emm_send,emm_as}.c; openair3/NAS/UE/EMM/Attach.c>
 _ATTACK_VDEF(int, bts_delay, 188)         // <delay between attacks; openair2/RRC/LTE/rrc_UE.c>
 _ATTACK_VDEF(int, blind_dos_attack, 0)    // <level information; openair2/RRC/LTE/MESSAGES/asn1_msg.c>
-_ATTACK_VDEF(uint64_t, tmsi_blind_dos_rrc, 0)  // <blind DoS RRC TMSI; openair2/RRC/LTE/MESSAGES/asn1_msg.c>
+_ATTACK_VDEF(long, tmsi_blind_dos_rrc, 0)  // <blind DoS RRC TMSI; openair2/RRC/LTE/MESSAGES/asn1_msg.c>
 _ATTACK_VDEF(int, dnlink_dos_attack, 0)   // <attack mode; openair2/RRC/LTE/rrc_UE.c; openair3/NAS/UE/EMM/SAP/emm_as.c>
 _ATTACK_VDEF(int, uplink_dos_attack, 0)   // <level information; openair2/RRC/LTE/rrc_UE.c>
 _ATTACK_VDEF(int, rep_conn_end_count, 0)  // <repeated connection setup complete attack count; openair2/RRC/LTE/rrc_UE.c>
